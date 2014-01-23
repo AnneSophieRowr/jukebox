f74bfc06ff4d451897a017d47e1bc78625eac46e
annesophie <annesophie@annesophie-alien.(none)>
2014-01-23 11:57:37 +0100

    import scripts

--- app/models/song.rb
+++ app/models/song.rb
@@ -27,22 +27,22 @@ class Song < ActiveRecord::Base
       songs = Dir.entries("public/temp/").reject! {|s| ['.', '..'].include? s or !s.include? 'mp3'}
 
       songs.each do |song|
-        begin
-          mp3 = Mp3Info.open("public/temp/#{song}") 
-        rescue Exception => e
-          puts e
+        mp3 = Mp3Info.open("public/temp/#{song}") 
+
+        infos = mp3.tag.empty? ? mp3.tag1 : mp3.tag 
+        unless infos.empty?
+          new_song = Song.new(name: infos.title.capitalize, album: infos.album.capitalize, user: user)
+
+          new_song.artist = Artist.find_or_create_by(name: infos.artist.downcase)
+
+          image = "public/temp/#{song.gsub('.mp3', '.jpg')}"
+          new_song.image = File.exist?(image) ? File.open(image) : infos.image
+
+          new_song.file = File.open("public/temp/#{song}")
+
+          song_exists = Song.where(name: new_song.name, album: new_song.album)
+          new_song.save! if song_exists.empty?
         end
-        new_song = Song.new(name: mp3.tag.title.capitalize, album: mp3.tag.album.capitalize, user: user)
-
-        new_song.artist = Artist.find_or_create_by(name: mp3.tag.artist.downcase)
-
-        image = "public/temp/#{song.gsub('.mp3', '.jpg')}"
-        new_song.image = File.exist?(image) ? File.open(image) : mp3.tag.image
-
-        new_song.file = File.open("public/temp/#{song}")
-
-        test = Song.where(name: new_song.name, album: new_song.album)
-        new_song.save! if test.empty?
       end
     rescue Exception => e
       puts e
