2225276df7e03e2350f11ea357fe617842fed6d2
annesophie <annesophie@annesophie-alien.(none)>
2014-03-24 12:05:07 +0100

    files transfer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,7 +20,8 @@ class ApplicationController < ActionController::Base
     artists = Artist.updated(date)
     albums = Album.updated(date)
     input_filenames = (songs.collect {|s| s.file.url} + songs.collect {|s| s.image.url} + artists.collect {|a| a.image.url} + albums.collect {|a| a.image.url}).reject! {|f| f.include? 'default.jpg'}
-    Zip::File.open('public/updated_files.zip', Zip::File::CREATE) do |zipfile|
+    zipfile_name = 'public/updated_files.zip'
+    Zip::File.open(zipfile_name, Zip::File::CREATE) do |zipfile|
       input_filenames.each do |filename|
         zipfile.add(filename[9, filename.length], 'public' + filename)
       end
