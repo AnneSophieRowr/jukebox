b739cc7f417359745e68a14bc26cd9edbcd0bfd1
annesophie <annesophie@annesophie-alien.(none)>
2014-03-26 13:06:18 +0100

    changing date format for synchronizer params

--- app/models/synchronizer.rb
+++ app/models/synchronizer.rb
@@ -2,7 +2,7 @@ class Synchronizer
 
   def self.data(date)
     data = {}
-    [Song, Playlist, PlaylistsSong, Kind, KindsPlaylist, Type, PlaylistsType, Album, AlbumsSong, Artist].each do |model|
+    [Song, Playlist, PlaylistsSong, Kind, KindsPlaylist, Type, PlaylistsType, Album, AlbumsSong, Artist, Parameter].each do |model|
       data[model.to_s.underscore.pluralize.capitalize] = model.updated(date) unless model.updated(date).empty?
     end
     data['keys'] = data.keys.collect {|k| k.capitalize} unless data.empty?
